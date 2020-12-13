9b5cf6a07accdce68180757b97bd52a6aeb2bd7c
monica88green <71725844+monica88green@users.noreply.github.com>
2020-12-13 14:47:57 -0700

    Added basic structure for weather app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
- import Loader from 'react-loader-spinner';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-       <h1>Hello from React Weather</h1>
-      </header>
-      <Loader
-         type="Puff"
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000} 
-      />
+      <form><input type="search" placeholder="Enter a City.." />
+      <input type="submit" value="Search" />
+      </form>
+      <h1>Denver</h1>
+      <h2>Last Updated: Sunday, 14:29</h2>
+      <ul className="weather-conditions">
+        <li>
+          Currently: 12 °C | °F
+        </li>
+        <li>
+          Sunny
+        </li>
+        <li>
+          Humidity: 5%
+        </li>
+        <li>
+          Windspeed: 0 km/h
+        </li>
+      </ul>
+     
     </div>
   );
 }
