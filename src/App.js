7093d8a80dc6b8050997816513682fc049bf928b
monica88green <71725844+monica88green@users.noreply.github.com>
2020-12-13 15:11:45 -0700

    Added bootstrap and github link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <form><input type="search" placeholder="Enter a City.." />
-      <input type="submit" value="Search" />
+      <form><div className="row">
+          <div className="col-md-9">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter a City"
+            />{" "}
+          </div>
+          <div className="col-md-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
       </form>
       <h1>Denver</h1>
       <h2>Last Updated: Sunday, 14:29</h2>
@@ -23,7 +37,16 @@ function App() {
           Windspeed: 0 km/h
         </li>
       </ul>
-     
+      <small className="github-link">
+        <a
+          href="https://github.com/monica88green/my-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Source Code{" "}
+        </a>
+        By Monica Ledesma
+      </small>
     </div>
   );
 }
