1d409dd7c3d2e95dc78752ffc75b2832a96b1b7a
monica88green <71725844+monica88green@users.noreply.github.com>
2020-12-14 16:40:55 -0700

    Linked search engine to weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('null');
+  const [temperature, setTemperature] = useState('null');
+  const [humidity, setHumidity] = useState('null');
+  const [windspeed, setWindspeed] = useState('null');
+  const [loaded, setLoaded] = useState(false);
+  
+  function showTemperature(response) {
+    setLoaded(true);
+    setTemperature(response.data.main.temp);
+    setHumidity(response.data.main.humidity);
+    setWindspeed(response.data.wind.speed);
+    }
 
-  let apiKey= "c2f91e6b37378ad6a4cd35f075bf9120";
-  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=${apiKey}`
-  axios.get(apiUrl).then(handleResponse);
   function handleSubmit(event) {
     event.preventDefault();
+      let apiKey= "c2f91e6b37378ad6a4cd35f075bf9120";
+      let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(showTemperature);
   }
   function updateCity(event) {
-    alert(event.target.value);
+    setCity(event.target.value);
   }
 
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit}><div className="row">
+  let form = <form onSubmit={handleSubmit}><div className="row">
           <div className="col-md-9">
             <input
               type="text"
@@ -35,20 +45,26 @@ function App() {
           </div>
         </div>
       </form>
-      <h1>Denver</h1>
+
+      if (loaded) {
+
+  return (
+    <div className="App">
+      {form}
+      <h1>{city}</h1>
       <h2>Last Updated: Sunday, 14:29</h2>
       <ul className="weather-conditions">
         <li>
-          Currently: 12 °C | °F
+          Currently: {Math.round(temperature)} °C | °F
         </li>
         <li>
           Sunny
         </li>
         <li>
-          Humidity: 5%
+          Humidity: {humidity}%
         </li>
         <li>
-          Windspeed: 0 km/h
+          Windspeed: {windspeed} km/h
         </li>
       </ul>
       <small className="github-link">
@@ -63,6 +79,9 @@ function App() {
       </small>
     </div>
   );
-}
+} else{
+ return <div>{form}
+  <h1>Where would you like to go?</h1></div>
+}}
 
 export default App;
